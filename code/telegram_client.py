190d0b03a7096d1f470ea686c4c65523ab5b0481
stonedpancake <slava2005qwe@gmail.com>
2021-04-08 00:19:25 +0300

    develop-1 (WORKING)

--- code/telegram_client.py
+++ code/telegram_client.py
@@ -9,23 +9,23 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.send_message(message.chat.id, "Howdy, how are you doing?")
 
 
 @bot.message_handler(content_types=["photo"])
 def beauty(message):
     file_info = bot.get_file(message.photo[len(message.photo) - 1].file_id)
     downloaded_file = bot.download_file(file_info.file_path)
-    downloaded_file_name = 'Loki.jpg'
-    src = '/Users/slavaderebchinskiy/Documents/python/Kaleidoscope/data/' + downloaded_file_name
+    src = '/Users/slavaderebchinskiy/Documents/python/Kaleidoscope/data/Photo.jpg'
 
     with open(src, 'wb') as new_file:
         new_file.write(downloaded_file)
-        new_file_name = 'res1.jpg'
-        PhotoFilters().white_black('../data/' + downloaded_file_name, '../data/' + new_file_name, 1.2)
 
-    photo = open('../data/' + new_file_name, 'rb')
-    bot.reply_to(message, "Фото добавлено")
+    PhotoFilters().white_black('../data/Photo.jpg', '../data/ResultPhoto.jpg', 1.2)
+
+    photo = open('../data/ResultPhoto.jpg', 'rb')
+
+    bot.send_message(message.chat.id, 'Фото добавлено')
     bot.send_photo(message.chat.id, photo)
 
 
